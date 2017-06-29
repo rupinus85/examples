560aa1037d2484844ef245e1b462888dcc193e41
강훈기(Hun-kee Kang)/Cloud Platform팀/SK <hunkee1017@sk.com>
2017-06-29 11:15:57 +0900

    update

---
+++ src/main/java/com/skcc/restcontroller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.skcc.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@RequestMapping("/")
+	public String helloworld(){
+		return "Hello World44";
+	}
+}
